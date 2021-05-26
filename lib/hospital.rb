6a957a77e3a7b62d65718163e9809feae03492f6
Michael Abbott <michael@mjabbottdesign.com>
2021-05-26 09:31:51 -0700

    Add Hospital readable attributes test and code

--- lib/hospital.rb
+++ lib/hospital.rb
@@ -1,7 +1,10 @@
 class Hospital
+  attr_reader :name, :cheif_surgery, :doctors
 
-  def initialize(*name, doctors)
+  def initialize(name, cheif_surgery, doctors)
     @name = name
+    @cheif_surgery = cheif_surgery
     @doctors = doctors
   end
+
 end
