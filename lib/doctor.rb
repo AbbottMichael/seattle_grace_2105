513bb6ce7851b5421b93c00ba2aff8a25766b1c0
Michael Abbott <michael@mjabbottdesign.com>
2021-05-26 08:56:29 -0700

    Add key value return tests and methods

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -5,4 +5,20 @@ attr_reader :attributes
     @attributes = attributes
   end
 
+  def name
+    @attributes[:name]
+  end
+
+  def specialty
+    @attributes[:specialty]
+  end
+
+  def education
+    @attributes[:education]
+  end
+
+  def salary
+    @attributes[:salary]
+  end
+
 end
--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -3,15 +3,26 @@ require './lib/doctor'
 RSpec.describe Doctor do
   describe 'Instantiation' do
     it 'exists' do
-      doctor = Doctor.new
+      meredith = Doctor.new
 # require'pry';binding.pry
-      expect(doctor).to be_an_instance_of(Doctor)
+      expect(meredith).to be_an_instance_of(Doctor)
     end
 
     it 'accepts attributes' do
-      doctor = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+      meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
       # require'pry';binding.pry
-      expect(doctor.attributes).to eq({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+      expect(meredith.attributes).to eq({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+    end
+
+    describe 'methods' do
+      it 'returns attribute key values' do
+      meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+
+      expect(meredith.name).to eq("Meredith Grey")
+      expect(meredith.specialty).to eq("General Surgery")
+      expect(meredith.education).to eq("Harvard University")
+      expect(meredith.salary).to eq(100_000)
+      end
     end
   end
 end
